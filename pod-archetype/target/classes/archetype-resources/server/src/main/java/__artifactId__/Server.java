71eede688ff7f9e35f0cf2e25df62fe5472bee41
ggordyn <ggordyn@itba.edu.ar>
2022-09-02 16:58:25 -0300

    archetype

---
+++ pod-archetype/target/classes/archetype-resources/server/src/main/java/__artifactId__/Server.java
@@ -0,0 +1,15 @@
+#set( $symbol_pound = '#' )
+#set( $symbol_dollar = '$' )
+#set( $symbol_escape = '\' )
+package ${package}.${artifactId};
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Server {
+    private static Logger logger = LoggerFactory.getLogger(Server.class);
+
+    public static void main(String[] args) {
+        logger.info("${rootArtifactId} Server Starting ...");
+    }
+}
