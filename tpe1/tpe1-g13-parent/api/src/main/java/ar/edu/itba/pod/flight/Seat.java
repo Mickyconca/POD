5f045539520e25e186fd9733f958411056d4222e
ggordyn <ggordyn@itba.edu.ar>
2022-09-08 13:19:11 -0300

    seat service progress

--- tpe1/tpe1-g13-parent/api/src/main/java/ar/edu/itba/pod/flight/Seat.java
+++ tpe1/tpe1-g13-parent/api/src/main/java/ar/edu/itba/pod/flight/Seat.java
@@ -12,7 +12,27 @@ public class Seat {
         this.isEmpty = true;
     }
 
+    public int getRowNumber() {
+        return rowNumber;
+    }
 
+    public void setRowNumber(int rowNumber) {
+        this.rowNumber = rowNumber;
+    }
 
+    public char getColLetter() {
+        return colLetter;
+    }
 
+    public void setColLetter(char colLetter) {
+        this.colLetter = colLetter;
+    }
+
+    public boolean isEmpty() {
+        return isEmpty;
+    }
+
+    public void setEmpty(boolean empty) {
+        isEmpty = empty;
+    }
 }
--- tpe1/tpe1-g13-parent/api/src/main/java/ar/edu/itba/pod/services/SeatService.java
+++ tpe1/tpe1-g13-parent/api/src/main/java/ar/edu/itba/pod/services/SeatService.java
@@ -1,5 +1,9 @@
 package ar.edu.itba.pod.services;
 
 public interface SeatService {
+    boolean isFree(String flightCode, int rowNumber, char colLetter, String passenger); //TODO borrar esto porque se que no va
+    void assignSeat(String flightCode, int rowNumber, char colLetter, String passenger);
+    void changePassengerSeat(String )
 
+    void changeFlightTicket(String flightCode, String passenger);
 }
